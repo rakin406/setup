f376a13db359c04c3526ccff7ea073a062abdcb2
rakin406 <rakinhacker@gmail.com>
2021-04-09 09:52:59 +0600

    Shorter script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Install programs(apt)
-sudo apt-get install bspwm sxhkd polybar alacritty rofi picom zsh feh ranger \
-    llvm clang python git fzf neofetch lolcat nnn lua
+sudo apt-get install bspwm sxhkd polybar rofi zsh ranger nitrogen llvm clang \
+    python git fzf neofetch lua firefox
 
 # Neovim source
 git clone https://github.com/neovim/neovim.git
@@ -12,12 +12,5 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Copy configuration files
-cp -r alacritty $HOME/.config
-cp -r bspwm $HOME/.config
-cp -r sxhkd $HOME/.config
-cp -r picom $HOME/.config
-cp -r ranger $HOME/.config
-cp -r polybar $HOME/.config
-cp -r nvim $HOME/.config
-cp -r wallpapers $HOME/Downloads
-cp .zshrc $HOME
+cp -r bspwm sxhkd ranger polybar nvim $HOME/.config/
+cp .zshrc .Xresources $HOME
