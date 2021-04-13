f23ab05aa77883a76c1488ce768c72fa73e64996
rakin406 <rakinhacker@gmail.com>
2021-04-13 13:10:58 +0600

    Install neovim faster

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@
 sudo apt-get install bspwm sxhkd polybar rofi zsh ranger nitrogen llvm clang \
     python git fzf neofetch lua firefox cmake make
 
-# Neovim source
-git clone https://github.com/neovim/neovim.git
+# Install latest version of neovim
+git clone https://github.com/rakin406/useful-scripts.git
+sudo ./useful-scripts/scripts/install-neovim.sh
 
 # Install neovim plugin manager(vim plug)
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
