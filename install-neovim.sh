9bd12a972bd0707fbad0d65b19aa780daaf4955f
Rakin Rahman <rakinhacker@gmail.com>
2022-06-30 20:34:57 +0600

    Install neovim with less or no errors

--- install-neovim.sh
+++ install-neovim.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# Install the latest version of neovim
 
+# Install the latest version of neovim
+sudo apt install clang build-essential ninja-build gettext libtool libtool-bin \
+    autoconf automake cmake g++ pkg-config unzip curl doxygen git
 sudo rm /usr/local/bin/nvim
 rm -rf neovim
 git clone https://github.com/neovim/neovim.git
@@ -9,3 +11,7 @@ make CMAKE_BUILD_TYPE=Release
 sudo make install
 cd ..
 rm -rf neovim
+
+# Install neovim plugin manager(vim plug)
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
--- setup.sh
+++ setup.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
+# Run with sudo
 
 # Install programs(apt)
-sudo apt-get install bspwm sxhkd polybar zsh ranger git
+apt install bspwm sxhkd polybar zsh ranger
 ./install-neovim.sh
-
-# Install neovim plugin manager(vim plug)
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Copy configuration files
 cp -r bspwm sxhkd ranger polybar nvim "$HOME"/.config/
 cp .zshrc .Xresources "$HOME"/
-
-mkdir -p "$HOME"/code
