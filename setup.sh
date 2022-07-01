f28ef4908bf218c9ca0abbed8f67ff217e7208fc
Rakin Rahman <rakinhacker@gmail.com>
2022-07-01 13:24:07 +0600

    Install polybar from git

--- setup.sh
+++ setup.sh
@@ -3,8 +3,24 @@
 
 # Install programs(apt)
 apt update && apt upgrade
-apt install git bspwm sxhkd polybar zsh ranger fonts-powerline
+apt install git python3 bspwm sxhkd zsh ranger fonts-powerline
 ./install-neovim.sh
+
+# Install all dependencies for polybar
+apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev \
+    libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev \
+    libxcb-xkb-dev pkg-config python3-xcbgen xcb-proto libxcb-xrm-dev i3-wm \
+    libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
+
+# additional dependencies
+apt install libxcb-composite0-dev libjsoncpp-dev clang build-essential python3-sphinx
+ln -s /usr/include/jsoncpp/json/ /usr/include/json
+
+# install polybar
+git clone https://github.com/jaagr/polybar.git
+cd polybar && ./build.sh
+cd ..
+rm -rf polybar
 
 # Copy configuration files
 cp -r bspwm sxhkd ranger polybar nvim "$HOME"/.config/
@@ -12,8 +28,7 @@ cp .zshrc .Xresources "$HOME"/
 
 # Install all powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
+cd fonts && ./install.sh
 # clean-up a bit
 cd ..
 rm -rf fonts
