802c600a0143cdf0129a28712849581e561ad0d6
Rakin Rahman <rakinhacker@gmail.com>
2022-07-01 12:32:03 +0600

    comment

--- install-neovim.sh
+++ install-neovim.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# Install the latest version of neovim
+# Install all dependencies
 sudo apt install clang build-essential ninja-build gettext libtool libtool-bin \
     autoconf automake cmake g++ pkg-config unzip curl doxygen git
+
+# Install the latest version of neovim
 sudo rm /usr/local/bin/nvim
 rm -rf neovim
 git clone https://github.com/neovim/neovim.git
