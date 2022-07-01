921b518c7b4a9178ee7bcd41b931a04293675a34
Rakin Rahman <rakinhacker@gmail.com>
2022-07-01 13:00:17 +0600

    Install all powerline fonts

--- setup.sh
+++ setup.sh
@@ -3,9 +3,17 @@
 
 # Install programs(apt)
 apt update && apt upgrade
-apt install bspwm sxhkd polybar zsh ranger
+apt install git bspwm sxhkd polybar zsh ranger fonts-powerline
 ./install-neovim.sh
 
 # Copy configuration files
 cp -r bspwm sxhkd ranger polybar nvim "$HOME"/.config/
 cp .zshrc .Xresources "$HOME"/
+
+# Install all powerline fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
