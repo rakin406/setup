6f4fb4a47323fb4ce2f8a3ec27969e054b1070ba
Rakin Rahman <rakinhacker@gmail.com>
2022-07-27 14:13:53 +0600

    update setup script

--- setup.sh
+++ setup.sh
@@ -13,12 +13,9 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
-# Install image previewer for ranger
-pip3 install ueberzug
-
 # Copy configuration files
-cp -r ranger nvim "$HOME"/.config/
-cp .zshrc .Xresources "$HOME"/
+cp -r nvim "$HOME"/.config/
+cp dotfiles/.zshrc dotfiles/.tmux.conf dotfiles/.Xresources "$HOME"/
 
 # Install all powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
