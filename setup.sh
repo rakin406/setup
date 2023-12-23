d12995a40abda526a6fdf807718346899005b1ca
rakin406 <rakinhacker@gmail.com>
2023-12-23 17:54:05 +0600

    feat: Update script

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ apt install neofetch lsd git zsh ranger fonts-powerline fzf nnn
 chsh -s $(which zsh)
 # install ohmyzsh
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.zsh/fast-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 # Copy configuration files
