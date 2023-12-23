bed03dfa2021a875769448d6257834884d7f1404
rakin406 <rakinhacker@gmail.com>
2023-12-23 17:51:20 +0600

    feat: Update script

--- setup.sh
+++ setup.sh
@@ -3,19 +3,19 @@
 
 # Install programs(apt)
 apt update && apt upgrade
-apt install git zsh ranger fonts-powerline fzf nnn
+apt install neofetch lsd git zsh ranger fonts-powerline fzf nnn
 ./scripts/install-neovim.sh
 
 # make zsh the default shell
 chsh -s $(which zsh)
 # install ohmyzsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 # Copy configuration files
 cp -r alacritty kitty nvim "$HOME"/.config/
-cp dotfiles/.zshrc dotfiles/.tmux.conf dotfiles/.Xresources "$HOME"/
+cp dotfiles/.zshrc dotfiles/.tmux.conf "$HOME"/
 
 # Install all powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
