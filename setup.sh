246aeba094eb13183726c940a10668fb6b11a4db
rakin406 <rakinhacker@gmail.com>
2023-12-25 23:15:42 +0600

    feat: Create symlinks instead of copy

--- setup.sh
+++ setup.sh
@@ -13,9 +13,20 @@ apt install clang build-essential ninja-build gettext libtool libtool-bin \
 chsh -s $(which zsh)
 # install ohmyzsh
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.zsh/fast-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting \
+    ~/.zsh/fast-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
-# Copy configuration files
-cp -r awesome picom alacritty kitty nvim "$HOME"/.config/
-cp dotfiles/.zshrc dotfiles/.tmux.conf "$HOME"/
+# Remove existing configurations
+rm ~/.tmux.conf ~/.zshrc
+rm -rf ~/.config/alacritty ~/.config/awesome ~/.config/kitty ~/.config/nvim \
+    ~/.config/picom
+
+# Create configuration symlinks
+ln -s $(pwd)/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s $(pwd)/dotfiles/.zshrc ~/.zshrc
+ln -s $(pwd)/alacritty ~/.config/alacritty
+ln -s $(pwd)/awesome ~/.config/awesome
+ln -s $(pwd)/kitty ~/.config/kitty
+ln -s $(pwd)/nvim ~/.config/nvim
+ln -s $(pwd)/picom ~/.config/picom
