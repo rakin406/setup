1e08d9fb785f9309dea7f77130b695d68c868e0d
rakin406 <rakinhacker@gmail.com>
2023-12-25 12:30:17 +0600

    feat: Remove fidget nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -38,9 +38,6 @@ return {
       -- Snippets
       'L3MON4D3/LuaSnip',
       'rafamadriz/friendly-snippets',
-
-      -- Useful status updates for LSP
-      { 'j-hui/fidget.nvim', opts = {} },
     },
   },
 
