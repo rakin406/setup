441cb082db7b79cfbb1c897686633201b5a1b50d
rakin406 <rakinhacker@gmail.com>
2023-12-28 16:27:50 +0600

    feat: Add trouble toggle

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>fe", vim.cmd.Lexplore)
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
