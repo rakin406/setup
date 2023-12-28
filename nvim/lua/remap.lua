ebbd90779386748cd0673dbd5dc4047af0de9248
rakin406 <rakinhacker@gmail.com>
2023-12-28 20:43:19 +0600

    feat: Add doxygen shortcut

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -2,5 +2,6 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>fe", vim.cmd.Lexplore)
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-vim.keymap.set("n", "<leader>a", vim.cmd.A)
+vim.keymap.set("n", "<leader>an", vim.cmd.A)
+vim.keymap.set("n", "<leader>dg", vim.cmd.Dox)
 vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
