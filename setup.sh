bf4ec9c7a0213df41e6b7ee3b721f90967ef10f8
rakin406 <rakinhacker@gmail.com>
2024-01-05 00:44:30 +0600

    refactor: Add quotes to prevent word splits

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ apt install clang build-essential ninja-build gettext libtool libtool-bin \
     autoconf automake cmake g++ pkg-config unzip curl doxygen git
 
 # make zsh the default shell
-chsh -s $(which zsh)
+chsh -s "$(which zsh)"
 # install ohmyzsh
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting \
@@ -23,10 +23,10 @@ rm -rf ~/.config/alacritty ~/.config/awesome ~/.config/kitty ~/.config/nvim \
     ~/.config/picom
 
 # Create configuration symlinks
-ln -s $(pwd)/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/dotfiles/.zshrc ~/.zshrc
-ln -s $(pwd)/alacritty ~/.config/alacritty
-ln -s $(pwd)/awesome ~/.config/awesome
-ln -s $(pwd)/kitty ~/.config/kitty
-ln -s $(pwd)/nvim ~/.config/nvim
-ln -s $(pwd)/picom ~/.config/picom
+ln -s "$(pwd)"/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s "$(pwd)"/dotfiles/.zshrc ~/.zshrc
+ln -s "$(pwd)"/alacritty ~/.config/alacritty
+ln -s "$(pwd)"/awesome ~/.config/awesome
+ln -s "$(pwd)"/kitty ~/.config/kitty
+ln -s "$(pwd)"/nvim ~/.config/nvim
+ln -s "$(pwd)"/picom ~/.config/picom
