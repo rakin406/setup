1ca68ee17acd89b6389ead5df404cb198857e58f
rakin406 <rakinhacker@gmail.com>
2024-01-11 00:47:34 +0600

    feat: Add 2 more mappings

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -2,11 +2,15 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>p", function() vim.cmd.Git("push") end)
 
 -- Rebase always
-vim.keymap.set("n", "<leader>P", function() vim.cmd.Git({ "pull", "--rebase" }) end)
+vim.keymap.set("n", "<leader>P", function() vim.cmd.Git("pull --rebase") end)
 
 -- This allows me to easily set the branch I am pushing and any tracking needed
 -- if I did not set the branch up correctly.
 vim.keymap.set("n", "<leader>t", ":Git push -u origin ")
+
+-- Easily add files
+vim.keymap.set("n", "<leader>af", ":Git add %<CR>")
+vim.keymap.set("n", "<leader>ae", ":Git add .<CR>")
 
 vim.keymap.set("n", "<leader>co", ":Git checkout ")
 vim.keymap.set("n", "<leader>ci", ":Git commit -m \"\"<Left>")
