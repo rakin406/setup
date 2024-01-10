c675a15bd747c4dbe7a97695cede05e774f97edd
rakin406 <rakinhacker@gmail.com>
2024-01-11 00:21:42 +0600

    feat: Add 3 more mappings mwuah

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,9 +1,13 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>p", function() vim.cmd.Git("push") end)
 
--- rebase always
+-- Rebase always
 vim.keymap.set("n", "<leader>P", function() vim.cmd.Git({ "pull", "--rebase" }) end)
 
--- NOTE: It allows me to easily set the branch i am pushing and any tracking
--- needed if i did not set the branch up correctly
+-- This allows me to easily set the branch I am pushing and any tracking needed
+-- if I did not set the branch up correctly.
 vim.keymap.set("n", "<leader>t", ":Git push -u origin ")
+
+vim.keymap.set("n", "<leader>co", ":Git checkout ")
+vim.keymap.set("n", "<leader>ci", ":Git commit -m \"\"<Left>")
+vim.keymap.set("n", "<leader>gt", function() vim.cmd.Git("status") end)
