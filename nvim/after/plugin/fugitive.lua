7520cc2be125aa7a7d47cf15339f89e2586335de
rakin406 <rakinhacker@gmail.com>
2024-01-10 23:54:13 +0600

    fix: Use anonymous function to call maps

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,29 +1,9 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>p", function() vim.cmd.Git("push") end)
 
-local my_fugitive = vim.api.nvim_create_augroup("my_fugitive ", {})
+-- rebase always
+vim.keymap.set("n", "<leader>P", function() vim.cmd.Git({ "pull", "--rebase" }) end)
 
-local autocmd = vim.api.nvim_create_autocmd
-autocmd("BufWinEnter", {
-  group = my_fugitive,
-  pattern = "*",
-  callback = function()
-    if vim.bo.ft ~= "fugitive" then
-      return
-    end
-
-    local bufnr = vim.api.nvim_get_current_buf()
-    local opts = { buffer = bufnr, remap = false }
-    vim.keymap.set("n", "<leader>p", function()
-      vim.cmd.Git("push")
-    end, opts)
-
-    -- rebase always
-    vim.keymap.set("n", "<leader>P", function()
-      vim.cmd.Git({ "pull", "--rebase" })
-    end, opts)
-
-    -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-    -- needed if i did not set the branch up correctly
-    vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
-  end,
-})
+-- NOTE: It allows me to easily set the branch i am pushing and any tracking
+-- needed if i did not set the branch up correctly
+vim.keymap.set("n", "<leader>t", ":Git push -u origin ")
