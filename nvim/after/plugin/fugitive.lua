8b7685934f845dcc15058a3243b32565ea26a79b
rakin406 <rakinhacker@gmail.com>
2024-01-17 00:02:49 +0600

    Remove some git mappings
    Empty description.

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -7,11 +7,3 @@ vim.keymap.set("n", "<leader>P", function() vim.cmd.Git("pull --rebase") end)
 -- This allows me to easily set the branch I am pushing and any tracking needed
 -- if I did not set the branch up correctly.
 vim.keymap.set("n", "<leader>t", ":Git push -u origin ")
-
--- Easily add files
-vim.keymap.set("n", "<leader>ga", ":Git add %<CR>")
-vim.keymap.set("n", "<leader>ge", ":Git add -A<CR>")
-
-vim.keymap.set("n", "<leader>co", ":Git checkout ")
-vim.keymap.set("n", "<leader>ci", ":Git commit -m \"\"<Left>")
-vim.keymap.set("n", "<leader>gt", function() vim.cmd.Git("status") end)
