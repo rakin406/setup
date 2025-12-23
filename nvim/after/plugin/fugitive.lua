5cd793c323c9f24aee8b6ffa58c0216b91706eef
Rakin Rahman <rakinhacker@gmail.com>
2025-12-23 09:51:09 +0600

    remove nvim setup

--- nvim/after/plugin/fugitive.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>p", function() vim.cmd.Git("push") end)
-
--- Rebase always
-vim.keymap.set("n", "<leader>P", function() vim.cmd.Git("pull --rebase") end)
-
--- This allows me to easily set the branch I am pushing and any tracking needed
--- if I did not set the branch up correctly.
-vim.keymap.set("n", "<leader>t", ":Git push -u origin ")
--- nvim/after/plugin/todo.lua
+++
@@ -1,9 +0,0 @@
-vim.keymap.set("n", "]t", function()
-  require("todo-comments").jump_next()
-end, { desc = "Next todo comment" })
-
-vim.keymap.set("n", "[t", function()
-  require("todo-comments").jump_prev()
-end, { desc = "Previous todo comment" })
-
-vim.keymap.set("n", "<leader>ft", vim.cmd.TodoTelescope)
